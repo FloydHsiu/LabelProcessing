e744965cc13219a7abe238c61e8f114ee1717d85
floydhsiu <s94040511s@gmail.com>
2018-05-29 16:30:53 +0800

    Add# setup.py for auto installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="LabelProcessing",
+    version="0.1",
+    classifiers = [
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+    author="FloydHsiu",
+    packages=['LabelProcessing'],
+    package_dir={'LabelProcessing': './LabelProcessing'},
+)
